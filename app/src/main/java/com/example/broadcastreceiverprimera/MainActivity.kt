45aadf89457d332bd22a77dcc234432052b98821
Mario Muñoz <snakeyes271101@gmail.com>
2023-12-04 19:12:05 +0100

    Práctica funcionando

--- app/src/main/java/com/example/broadcastreceiverprimera/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiverprimera/MainActivity.kt
@@ -4,38 +4,76 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var wifiText: TextView
+    // Declaración variables
+    private lateinit var textoRed: TextView
+    private lateinit var connectivityManager: ConnectivityManager
 
-    private val wifiReceiver = object : BroadcastReceiver() {
-
-        /**
-         *
-         *
-         * @param context
-         * @param intent
-         */
-        override fun onReceive(context: Context?, intent: Intent?) {
-
+    // BroadcastReceiver para manejar cambios en la conectividad de red
+    private val estadoRedReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            actualizarTipoRed() // Llamo al método para comprobar tipo conexión
         }
-
     }
 
-
+    /**
+     *
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        wifiText = findViewById(R.id.wifiText)
+        // Inicializar variables
+        textoRed = findViewById(R.id.wifiText)
+        connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        registerReceiver(wifiReceiver, IntentFilter())
+        // Registrar el BroadcastReceiver para cambios en la conectividad de red
+        registerReceiver(estadoRedReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
 
-
+        actualizarTipoRed() // Para actualizar el estado nada más abrir
     }
+
+    /**
+     * Cuando se destruye la actividad, se "desuscribe" del broadcast
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(estadoRedReceiver)
+    }
+
+
+    /**
+     * Actualiza el estado de la red
+     */
+    private fun actualizarTipoRed() {
+        // Obtener las capacidades de la red activa
+        val networkCapabilities = connectivityManager.activeNetwork?.let {
+            connectivityManager.getNetworkCapabilities(it)
+        }
+
+        // Determinar el estado de la red según las capacidades
+        val status: String = when {
+
+            // networkCapabilities son las "capacidades de la red", la "?" sirve para que solo llame al hasTransport si no es nulo
+            // hasTransport sirve para comprobar que tipo de red tiene, devuelve un booleano
+            networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true -> {
+                "Conectado a red Wi-Fi"
+            }
+
+            networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true -> {
+                "Conectado a datos móviles"
+            }
+
+            else -> "No estás conectado a ninguna red"
+        }
+        textoRed.text = status // Cambiar texto según conexión
+    }
+
 }
